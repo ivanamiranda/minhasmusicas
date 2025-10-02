c0ce2ffad98f1216335064aadec0814ce45c733a
Ivana Miranda <ivanagv@gmail.com>
2025-10-02 01:10:20 -0300

    feat: instanciando as classes Musica e Podcast

--- src/br/com/minhasmusicas/principal/Main.java
+++ src/br/com/minhasmusicas/principal/Main.java
@@ -1,7 +1,37 @@
 package br.com.minhasmusicas.principal;
 
+import br.com.minhasmusicas.modelos.Musica;
+import br.com.minhasmusicas.modelos.Podcast;
+
 public class Main {
     public static void main(String[] args) {
 
+        //Instanciando musica
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduzir();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curtida();
+        }
+
+        //Instanciando Podcast
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcos Mendes");
+
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduzir();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curtida();
+        }
     }
 }
